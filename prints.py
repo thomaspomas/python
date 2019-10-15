f981f97c46d01aff255c3717a62dee452588be4b
Thomas Lundberg <44287434+thomaspomas@users.noreply.github.com>
2019-10-15 10:28:55 +0200

    Create prints.py

---
+++ prints.py
@@ -0,0 +1,14 @@
+print(1,2,3,4)
+# Output: 1 2 3 4
+
+print(1,2,3,4,sep='*')
+# Output: 1*2*3*4
+
+print(1+2+3+4)
+# Output the sum of 1+2+3+4 = 10
+
+print("1+2+3+4")
+# Output the string 1+2+3+4
+
+print(1,2,3,4,sep='#',end='&')
+# Output: 1#2#3#4&
