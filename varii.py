bd2c9c0e03b943c8b2e4f1e6bbf9fe2bcc1cb107
thomaspomas <44287434+thomaspomas@users.noreply.github.com>
2019-10-15 09:41:03 +0200

    Create varii.py

---
+++ varii.py
@@ -0,0 +1,16 @@
+v1=5
+#skapar variabel med identifier v1 och ger den value 5
+v2=6
+#skapar variabel med identifier v2 och ger den value 6
+v3="5"
+#skapar variabel med identifier v3 och ger den value "5"
+v4="6"
+#skapar variabel med identifier v4 och ger den value "6"
+
+
+print (v1 + v2)
+#returnerar v1 och v2 
+
+print (v3 + v4)
+#reurnerar v3 och v4 
+#vad skiljer v1 samt v2 fran v3 och v4? 
